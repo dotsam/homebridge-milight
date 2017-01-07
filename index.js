95a9340618fcd12955a22feabe065545dbaf0666
Sam Edwards <sam@samedwards.ca>
2017-01-07 14:56:11 -0800

    Rethink the concept of bridges and zones and bulbs. Now we can have multiple types of bulbs per bridge object, which means we're actually using command queuing properly

--- index.js
+++ index.js
@@ -28,16 +28,14 @@ MiLightPlatform.prototype.accessories = function (callback) {
     if (this.config.bridges.length === 0) {
       this.log.error("No bridges found in configuration.");
     } else {
-      for (var i = 0; i < this.config.bridges.length; i++) {
-        if (this.config.bridges[i]) {
-          var returnedZones = this._addLamps(this.config.bridges[i]);
-          Array.prototype.push.apply(foundZones, returnedZones);
-        }
+      for (var bridge in this.config.bridges) {
+        var returnedZones = this._addLamps(this.config.bridges[bridge]);
+        Array.prototype.push.apply(foundZones, returnedZones);
       }
     }
 
   } else if (this.config.zones) {
-    this.log.warn("DEPRECATED: See README for details of setting zones in new bridges key.");
+    this.log.warn("DEPRECATED: See README for details of setting multiple lights in new bridges key.");
     foundZones = this._addLamps(this.config);
   } else {
     this.log.error("Could not read any zones/bridges from configuration.");
@@ -46,45 +44,20 @@ MiLightPlatform.prototype.accessories = function (callback) {
   if (foundZones.length > 0) {
     callback(foundZones);
   } else {
-    this.log.error("Unable to find any valid zones.");
+    this.log.error("Unable to find any valid lights.");
   }
 
 };
 
 MiLightPlatform.prototype._addLamps = function (bridgeConfig) {
   var zones = [];
-  var zonesLength = 0;
+  var bridgeController;
 
-  // Various error checking
-  if (bridgeConfig.zones) {
-    zonesLength = bridgeConfig.zones.length;
-  } else {
-    this.log.error("Could not read zones from configuration.");
-  }
-
-  if (!bridgeConfig.type || ['fullColor', 'rgbw', 'rgb', 'white'].indexOf(bridgeConfig.type) === -1) {
-    this.log.warning("Type not specified or invalid type, defaulting to rgbw");
-    bridgeConfig.type = "rgbw";
-  }
-
-  if (zonesLength === 0) {
-    this.log.error("No zones found in configuration.");
-  } else if (bridgeConfig.type === "rgb" && zonesLength > 1) {
-    this.log.warning("RGB lamps only have a single zone. Only the first defined zone will be used.");
-    zonesLength = 1;
-  } else if (zonesLength > 4) {
-    this.log.warning("Only a maximum of 4 zones are supported per bridge. Only recognizing the first 4 zones.");
-    zonesLength = 4;
-  }
-  
-  if (!bridgeConfig.hasOwnProperty('version')) {
+  // Setting appropriate commands per bridge version. Defaults to v2 as those are the commands that previous versions of the plugin used
+  if (!bridgeConfig.version) {
     bridgeConfig.version = "v2";
   }
-  
-  if (bridgeConfig.version !== "v6" && bridgeConfig.type === "fullColor") {
-    this.log.error("Full colour bulbs only avaliable with v6 bridge!");
-  }
-  
+
   if (bridgeConfig.version === "v6") {
     bridgeConfig.commands = require("node-milight-promise").commandsV6;
   } else if (bridgeConfig.version === "v3") {
@@ -93,22 +66,74 @@ MiLightPlatform.prototype._addLamps = function (bridgeConfig) {
     bridgeConfig.commands = require("node-milight-promise").commands;
   }
 
-  // Initialize a new controller to be used for all zones defined for this bridge
-  var bridgeController = new Milight({
-    ip: bridgeConfig.ip_address,
-    port: bridgeConfig.port,
-    delayBetweenCommands: bridgeConfig.delay,
-    commandRepeat: bridgeConfig.repeat,
-    type: bridgeConfig.version
-  });
+  if (!bridgeConfig.zones && !bridgeConfig.lights) {
+    this.log.error("No lights or zones defined for this bridge. Check your configuration.");
+  } else {
+    if (bridgeConfig.lights) {
+      if (bridgeConfig.zones) {
+        this.log.warn("Zones and Lights keys both detected for bridge. Using only Lights key.");
+      }
+    } else if (bridgeConfig.zones) {
+      this.log.warn("DEPRECATED: See README for details of setting multiple light types per bridge key.");
 
-  // Create lamp accessories for all of the defined zones
-  for (var i = 0; i < zonesLength; i++) {
-    if (bridgeConfig.zones[i]) {
-      bridgeConfig.name = bridgeConfig.zones[i];
-      bridgeConfig.zone = i + 1;
-      var lamp = new MiLightAccessory(this.log, bridgeConfig, bridgeController);
-      zones.push(lamp);
+      if (!bridgeConfig.type) {
+        this.log.warn("Type not specified, defaulting to rgbw");
+        bridgeConfig.type = "rgbw";
+      }
+
+      bridgeConfig.lights = {};
+      bridgeConfig.lights[bridgeConfig.type] =  bridgeConfig.zones;
+    }
+
+    if (Object.keys(bridgeConfig.lights).length > 0) {
+
+      for (var lightType in bridgeConfig.lights) {
+        if (["fullColor", "rgbw", "rgb", "white"].indexOf(lightType) === -1) {
+          this.log.error("Invalid light type specified.");
+        } else if (bridgeConfig.version !== "v6" && lightType === "fullColor") {
+          this.log.error("Full colour bulbs only avaliable with v6 bridge!");
+        } else {
+          var zonesLength = bridgeConfig.lights[lightType].length;
+
+          if (zonesLength < 1) {
+            this.log.error("No zones found in configuration.");
+            zonesLength = 0;
+          } else if (lightType === "rgb" && zonesLength > 1) {
+            this.log.warn("RGB lamps only have a single zone. Only the first defined zone will be used.");
+            zonesLength = 1;
+          } else if (zonesLength > 4) {
+            this.log.warn("Only a maximum of 4 zones are supported per bridge. Only recognizing the first 4 zones.");
+            zonesLength = 4;
+          }
+
+          if (zonesLength > 0) {
+            // If it hasn't been already, initialize a new controller to be used for all zones defined for this bridge
+            if (typeof(bridgeController) != "object") {
+              bridgeController = new Milight({
+                ip: bridgeConfig.ip_address,
+                port: bridgeConfig.port,
+                delayBetweenCommands: bridgeConfig.delay,
+                commandRepeat: bridgeConfig.repeat,
+                type: bridgeConfig.version
+              });
+            }
+
+            // Create lamp accessories for all of the defined zones
+            for (var i = 0; i < zonesLength; i++) {
+              if (bridgeConfig.name = bridgeConfig.lights[lightType][i]) {
+                bridgeConfig.type = lightType;
+                bridgeConfig.zone = i + 1;
+                var lamp = new MiLightAccessory(this.log, bridgeConfig, bridgeController);
+                zones.push(lamp);
+              } else if (bridgeConfig.lights[lightType][i] !== null) {
+                this.log.error("Unable to add light.");
+              }
+            }
+          }
+        }
+      }
+     } else {
+      this.log.error("Could not read any zones/lights from configuration.");
     }
   }
 
@@ -123,14 +148,14 @@ function MiLightAccessory(log, lampConfig, lampController) {
   this.zone = lampConfig.zone;
   this.type = lampConfig.type;
   this.version = lampConfig.version;
-  
+
   // have to keep track of the last values we set brightness and colour temp to for rgb/white bulbs
   this.brightness = -1;
   this.hue = -1;
 
   // assign to the bridge
   this.light = lampController;
-  
+
   // use the right commands for this bridge
   this.commands = lampConfig.commands;
 
@@ -174,7 +199,7 @@ MiLightAccessory.prototype.setBrightness = function (level, callback) {
       } else {
         this.light.sendCommands(this.commands[this.type].brightness(level));
       }
-    } else {      
+    } else {
       // If this is an rgb or a white lamp, they only support brightness up and down.
       if (this.type === "white" && level === 100) {
         // But the white lamps do have a "maximum brightness" command
@@ -183,20 +208,20 @@ MiLightAccessory.prototype.setBrightness = function (level, callback) {
       } else {
         // We're going to send the number of brightness up or down commands required to get to get from
         // the current value that HomeKit knows to the target value
-        
+
         // Keeping track of the value separately from Homebridge so we know when to change across multiple small adjustments
         if (this.brightness === -1) this.brightness = this.lightbulbService.getCharacteristic(Characteristic.Brightness).value;
         var currentLevel = this.brightness;
-        
+
         var targetLevel = level - currentLevel;
         var targetDirection = Math.sign(targetLevel);
         targetLevel = Math.max(0,(Math.round(Math.abs(targetLevel)/10))); // There are 10 steps of brightness
-          
+
         if (targetDirection === 0 || targetDirection === -0 || targetLevel === 0) {
           this.log("[" + this.name + "] Change not large enough to move to next step for bulb");
         } else {
           this.brightness = level;
-          
+
           for (; targetLevel !== 0; targetLevel--) {
             if (targetDirection === 1) {
               this.light.sendCommands(this.commands[this.type].brightUp(this.zone));
@@ -227,7 +252,7 @@ MiLightAccessory.prototype.setHue = function (value, callback) {
       this.light.sendCommands(this.commands[this.type].whiteMode(this.zone));
     } else {
       this.hue = value;
-      
+
       if (this.version === "v6") {
         this.light.sendCommands(this.commands[this.type].hue(this.zone, helper.hsvToMilightColor(hue), true));
       } else {
@@ -242,20 +267,20 @@ MiLightAccessory.prototype.setHue = function (value, callback) {
     }
   } else if (this.type === "white") {
     // Again, white lamps don't support setting an absolue colour temp, so we'll do some math to figure out how to get there
-    
+
     // Keeping track of the value separately from Homebridge so we know when to change across multiple small adjustments
     if (this.hue === -1) this.hue = this.lightbulbService.getCharacteristic(Characteristic.Hue).value;
     var currentLevel = this.hue;
-    
+
     var targetLevel = value - currentLevel;
     var targetDirection = Math.sign(targetLevel);
     targetLevel = Math.max(0,(Math.round(Math.abs(targetLevel)/36))); // There are 10 steps of colour temp (360/10)
-      
+
     if (targetDirection === 0 || targetDirection === -0 || targetLevel === 0) {
       this.log("[" + this.name + "] Change not large enough to move to next step for bulb");
     } else {
       this.hue = value;
-      
+
       for (; targetLevel !== 0; targetLevel--) {
         if (targetDirection === 1) {
           this.light.sendCommands(this.commands[this.type].cooler(this.zone));
@@ -275,13 +300,13 @@ MiLightAccessory.prototype.setSaturation = function (value, callback) {
     if (value === 0) {
       // Send on command to ensure we're addressing the right bulb
       this.lightbulbService.setCharacteristic(Characteristic.On, true);
-      
+
       this.log("[" + this.name + "] Saturation set to 0, setting bulb to white");
       this.light.sendCommands(this.commands[this.type].whiteMode(this.zone));
     } else if (this.lightbulbService.getCharacteristic(Characteristic.Hue).value !== 0) {
       // Send on command to ensure we're addressing the right bulb
       this.lightbulbService.setCharacteristic(Characteristic.On, true);
-      
+
       this.log.info("[" + this.name + "] Saturation set to %s, but hue is not 0, resetting hue", value);
       this.light.sendCommands(this.commands[this.type].hue(helper.hsvToMilightColor([this.lightbulbService.getCharacteristic(Characteristic.Hue).value, 0, 0])));
     } else {
@@ -290,10 +315,10 @@ MiLightAccessory.prototype.setSaturation = function (value, callback) {
   } else if (this.type === "fullColor"){
     // Send on command to ensure we're addressing the right bulb
     this.lightbulbService.setCharacteristic(Characteristic.On, true);
-    
+
     this.log("[" + this.name + "] Setting saturation to %s", value);
     this.light.sendCommands(this.commands[this.type].saturation(this.zone, value));
-    
+
   } else {
     this.log.info("[" + this.name + "] Setting saturation to %s (NOTE: No impact on %s %s bulbs)", value, this.type, this.log.prefix);
   }
